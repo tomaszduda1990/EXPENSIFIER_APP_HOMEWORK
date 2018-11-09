b70ebc3a04435301ab793be31d58ff5614cb06b3
tomaszduda1990 <tomasz.duda.krk@gmail.com>
2018-11-09 16:22:15 +0100

    adding styles

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -2,9 +2,14 @@ import React from "react";
 import { connect } from "react-redux";
 import { startLogin } from "../actions/auth";
 export const LoginPage = ({ startLogin }) => (
-  <div>
-    <p>Please login</p>
-    <button onClick={startLogin}>Login</button>
+  <div className="box-layout">
+    <div className="box-layout__box">
+      <h1 className="box-layout__title">Expensifier</h1>
+      <p>Keep your expenses under control!</p>
+      <button className="button" onClick={startLogin}>
+        Login with Google
+      </button>
+    </div>
   </div>
 );
 
